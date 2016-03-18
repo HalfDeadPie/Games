9647067a3a11721fcc36816de055bde15df99008
JaroslavLisiak <jaroslav.lisiakvgmail.com>
2016-03-18 18:05:25 +0100

    asdasdasdas

--- app/src/main/java/com/example/simon/gamesshop/Connector.java
+++ app/src/main/java/com/example/simon/gamesshop/Connector.java
@@ -21,14 +21,12 @@ public class Connector extends AsyncTask<String, String, String> {
         try {
 
             URL url = new URL(ourURL);
-            HttpURLConnection connection =
-                    (HttpURLConnection) url.openConnection();
+            HttpURLConnection connection =(HttpURLConnection) url.openConnection();
 
-            connection.addRequestProperty("application-id",
-                    "94B456C3-9A44-D044-FF87-A1D0AA589D00");
-            connection.addRequestProperty("secret-key",
-                    "CDA1E692-BF29-7396-FF7F-0E699E669000");
-
+            connection.addRequestProperty("application-id","94B456C3-9A44-D044-FF87-A1D0AA589D00");
+            connection.addRequestProperty("secret-key","CDA1E692-BF29-7396-FF7F-0E699E669000");
+            String randomstringForTesting = null;
+            String testingStringForPush = null;
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(connection.getInputStream()));
             StringBuffer json = new StringBuffer(1024);
