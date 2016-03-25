a5d4520da410f1a511f3159bd8f59dfd1b863d9b
JaroslavLisiak <jaroslav.lisiakvgmail.com>
2016-03-25 11:45:15 +0100

    login + main obrazovka upravena po stlaceni tlacidla presmeruje na login activity v login activity meno: Jaro heslo: Simon malo by napisat Prihlasenie uspesne

--- app/src/main/java/com/example/simon/gamesshop/Login.java
+++ app/src/main/java/com/example/simon/gamesshop/Login.java
@@ -1,9 +1,13 @@
 package com.example.simon.gamesshop;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
@@ -11,22 +15,35 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Hide the Title Bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_login);
+        // Hide the Status Bar (signal, battery, time)
+        // getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //         WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
     public void testLogin(View view){
-        String ok_name = "Jaro";
-        String ok_password = "Simon";
+
         TextView name = (TextView) findViewById(R.id.login_name);
         TextView password = (TextView) findViewById(R.id.login_passwd);
         TextView error = (TextView) findViewById(R.id.login_error);
 
-        if((name.getText().toString().equals("Jaro")) && (password.getText().toString().equals("Simon"))){
+        if((name.getText().toString().equals("a")) && (password.getText().toString().equals("a"))){
+            // uspesne zadanie mena a hesla
+
             error.setText("Prihlasenie je uspesne!");
+            // farba nefunguje
+            error.setLinkTextColor(Color.GREEN);
+
+            Intent intent = new Intent(this, detail.class);
+            startActivity(intent);
             //System.out.println("Som tu1!");
         }else{
-            assert error != null;
-            System.out.println("Som tu2!");
+            // nespravne zadane meno a heslo
             error.setText("Nespravne meno alebo heslo!");
         }
 
