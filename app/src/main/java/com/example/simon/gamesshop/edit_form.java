bc0c0af1399d2663cf71d78daf4eed71ff5bd4a5
Jaroslav Lisiak <jaroslav.lisiak@gmail.com>
2016-04-17 18:32:19 +0200

    pridanie kontroly ci su vyplnene vsetky polia v edite upravenie zobrazenie datumu

--- app/src/main/java/com/example/simon/gamesshop/edit_form.java
+++ app/src/main/java/com/example/simon/gamesshop/edit_form.java
@@ -1,10 +1,12 @@
 package com.example.simon.gamesshop;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 /**
@@ -38,16 +40,25 @@ public class edit_form extends AppCompatActivity {
     }
 
     public void sendEdit(View view){
-        Connector con = new Connector(this);
-        TextView idView = (TextView) findViewById(R.id.edit_form_uid);
 
-        String id = idView.getText().toString();
-        con.execute("SENDEDIT",id);
-    /*
+
+
+        RadioGroup groupGenre = (RadioGroup) findViewById(R.id.groupGenre);
+        RadioGroup groupPlatform = (RadioGroup) findViewById(R.id.groupPlatform);
+
+        int radioButtonID = groupGenre.getCheckedRadioButtonId();
+        View radioButton = groupGenre.findViewById(radioButtonID);
+        int GenreID = groupGenre.indexOfChild(radioButton);     // cislo zanru
+
+        int radioButtonIDP = groupPlatform.getCheckedRadioButtonId();
+        View radioButtonP = groupPlatform.findViewById(radioButtonIDP);
+        int PlatformID = groupPlatform.indexOfChild(radioButtonP);  // cislo platformy
+
+
+
         TextView detail_description = (TextView) findViewById(R.id.edit_form_decription);
         TextView detail_name = (TextView) findViewById(R.id.edit_form_title);
         TextView detail_image = (TextView) findViewById(R.id.edit_form_image);
-        //ExpandableListView detail_pegi = (ExpandableListView) findViewById(R.id.edit_form_pegi_list);
         TextView detail_pegi =(TextView) findViewById(R.id.edit_form_pegi);
         TextView detail_rating = (TextView) findViewById(R.id.edit_form_rating);
         TextView detail_price = (TextView) findViewById(R.id.edit_form_price);
@@ -55,25 +66,58 @@ public class edit_form extends AppCompatActivity {
         TextView detail_count = (TextView) findViewById(R.id.edit_form_count);
         TextView detail_producer = (TextView) findViewById(R.id.edit_form_producer);
         TextView detail_language = (TextView) findViewById(R.id.edit_form_languages);
+        TextView detail_uid = (TextView) findViewById(R.id.edit_form_uid);
 
-        RadioButton g0 = (RadioButton) findViewById(R.id.radioButtonAction);
-        RadioButton g1 = (RadioButton) findViewById(R.id.radioButtonAdventure);
-        RadioButton g2 = (RadioButton) findViewById(R.id.radioButtonCasual);
-        RadioButton g3 = (RadioButton) findViewById(R.id.radioButtonIndie);
-        RadioButton g4 = (RadioButton) findViewById(R.id.radioButtonMM);
-        RadioButton g5 = (RadioButton) findViewById(R.id.radioButtonRacing);
-        RadioButton g6 = (RadioButton) findViewById(R.id.radioButtonRPG);
-        RadioButton g7 =(RadioButton)  findViewById(R.id.radioButtonSimulation);
-        RadioButton g8 = (RadioButton) findViewById(R.id.radioButtonSports);
-        RadioButton g9 = (RadioButton) findViewById(R.id.radioButtonStrategy);
 
-        RadioButton p0 = (RadioButton) findViewById(R.id.radioButtonPC);
-        RadioButton p1 = (RadioButton) findViewById(R.id.radiobutton4PS3);
-        RadioButton p2 = (RadioButton) findViewById(R.id.radioButton3ONE);
-        RadioButton p3 = (RadioButton) findViewById(R.id.radioButton2_Wii);
-        RadioButton p4 = (RadioButton) findViewById(R.id.radioButton5PS4);
-        RadioButton p5 = (RadioButton) findViewById(R.id.radioButton6360);
-        */
+        Game g = new Game();
+        g.setName(detail_name.getText().toString());
+        g.setDescription(detail_description.getText().toString());
+        g.setPegi(detail_pegi.getText().toString());
+        g.setRating(Integer.parseInt(detail_rating.getText().toString()));
+        g.setImage(detail_image.getText().toString());
+        g.setPrice(Integer.parseInt(detail_price.getText().toString()));
+        g.setReleaseDate(detail_date.getText().toString());
+        g.setCount(Integer.parseInt(detail_count.getText().toString()));
+        g.setProducer(detail_producer.getText().toString());
+        g.setLanguage(detail_language.getText().toString());
+        g.setGenre(GenreID);
+        g.setPlatform(PlatformID);
+        g.setUID(detail_uid.getText().toString());
 
+        if(isEmpty(g)){
+            AlertDialog.Builder dlgAlert = new AlertDialog.Builder(this);
+            dlgAlert.setMessage("Please fill all required fields!");
+            dlgAlert.setTitle("Error!");
+            dlgAlert.setCancelable(true);
+            dlgAlert.create().show();
+        }else {
+
+            Connector con = new Connector(this);
+            TextView idView = (TextView) findViewById(R.id.edit_form_uid);
+
+            String id = idView.getText().toString();
+            con.execute("SENDEDIT", id);
+        }
+
+    }
+
+
+    private boolean isEmpty(Game g) {
+        if(     g.getGenre() == -1 ||
+                g.getPlatform() == -1 ||
+                g.getCount() <0 ||
+                g.getDescription().equals("") ||
+                g.getImage().equals("") ||
+                g.getLanguage().equals("") ||
+                g.getName().equals("") ||
+                g.getPegi().equals("") ||
+                g.getPrice() < 0 ||
+                g.getReleaseDate().equals("") ||
+                g.getRating() < 0 ||
+                g.getProducer().equals("")){
+            return  true;
+        }else{
+            return false;
+        }
     }
 }
